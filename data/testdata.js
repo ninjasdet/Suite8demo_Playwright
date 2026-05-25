8ec1a40fc08b0273946c349aea275ca87da75e45
Adhithyasankari Maruthachalam <adhithyanavaneeth@gmail.com>
2026-05-24 23:22:03 -0400

    Updated the correct testdata file

---
+++ data/testdata.js
@@ -0,0 +1,12 @@
+module.exports = {
+  username: "will",
+  password: "will",
+  firstName: "John",
+  lastName: "Smith",
+  mobile: "4441234567",
+  phone: "9876543210",
+  department: "IT",
+  email: "john.smith@example.com",
+  contactfullname: "John Smith",
+  leadfullname: "Lee Clausen"
+};
