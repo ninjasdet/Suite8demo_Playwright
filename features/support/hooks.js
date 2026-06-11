5e757d92d151f63083ca3a433655dd95b74cbd91
anushan23 <anushan23@gmail.com>
2026-06-10 20:20:28 -0500

    update tests

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -5,7 +5,7 @@ const {POManager} = require('../../pageobjects/POManager.js');
 
 
 Before(async function () {
-    this.browser = await chromium.launch({ headless: false });
+    this.browser = await chromium.launch({ headless: true });
     this.context = await this.browser.newContext();
     this.page = await this.context.newPage();
     this.poManager = new POManager(this.page);
